677b6449a79e1013b67893958d178aa9ef38589b
Eunhak Lee <lee@enak.kr>
2021-04-27 06:01:32 +0900

    feat: kakaowork wrapper로 API 관리

---
+++ libs/kakaoWork/wrapper.js
@@ -0,0 +1,19 @@
+const Config = require('../../configs/app/development');
+const { KakaoWork } = require('kakaowork');
+
+
+const api = new KakaoWork(Config.keys.kakaoWork.bot);
+(async () => {
+    let conv = 0;
+    let user = 0;
+    for await (const c of api.fetchConversationList())
+        conv += 1;
+    for await (const u of api.fetchUserList())
+        user += 1;
+
+    console.log(`Fetched ${user} Users and ${conv} Conversations`)
+})();
+
+module.exports = {
+    api
+};
