879724cc719db72c05e701af4185f0c075481275
Eunhak Lee <lee@enak.kr>
2021-04-27 06:01:50 +0900

    feat: 메시지 템플릿 생성

--- handler/qna/index.js
+++ handler/qna/index.js
@@ -1,5 +1,12 @@
 const libKakaoWork = require('../../libs/kakaoWork');
+const { api } = require('../../libs/kakaoWork/wrapper');
+const { SelectOption } = require('kakaowork');
 const messages = require("./messages");
+
+const mapper = (obj) => {
+	const map = new Map();
+	return Object.fromEntries(obj.serialize(map));
+}
 
 exports.handleRequest = async (req, res, next) => {
 	const { message, value } = req.body;
@@ -9,68 +16,19 @@ exports.handleRequest = async (req, res, next) => {
 	switch (parsedValue.action) {
 		case "modal_all_posts":
 			res.json({
-				view: {
-					title: 'QnA 전체 게시글',
-					accept: '확인',
-					decline: '취소',
-					value: `{"type": "qna", "action": "modal_all_posts"}`,
-					blocks: [
-						{
-							type: 'label',
-							text: '전체 게시글',
-							markdown: true
-						},
-						{
-							type: 'select',
-							name: 'open_post',
-							required: true,
-							placeholder: '게시글 선택',
-							options: [
-								{
-									text: 'TypeScript 에서 오류가 나요',
-									value: '1'
-								},
-								{
-									text: 'eslint를 꼭 써야할까요',
-									value: '2'
-								}
-							]
-						}
-					]
-				}
+				view: mapper(messages.blockPresets.modal_all_posts([
+					new SelectOption('게시글 1', '1'),
+					new SelectOption('게시글 2', '2'),
+					new SelectOption('게시글 3', '3')
+				]))
 			});
 			return;
 		case "modal_my_posts":
 			res.json({
-				view: {
-					title: 'QnA 내 게시글',
-					accept: '확인',
-					decline: '취소',
-					value: `{"type": "qna", "action": "modal_all_posts"}`,
-					blocks: [
-						{
-							type: 'label',
-							text: '내 게시글',
-							markdown: true
-						},
-						{
-							type: 'select',
-							name: 'open_post',
-							required: true,
-							placeholder: '게시글 선택',
-							options: [
-								{
-									text: 'TypeScript 에서 오류가 나요',
-									value: '1'
-								},
-								{
-									text: 'eslint를 꼭 써야할까요',
-									value: '2'
-								}
-							]
-						}
-					]
-				}
+				view: mapper(messages.blockPresets.modal_my_posts([
+					new SelectOption('게시글 1', '1'),
+					new SelectOption('게시글 3', '3')
+				]))
 			});
 			return;
 		default:
@@ -79,20 +37,25 @@ exports.handleRequest = async (req, res, next) => {
 };
 
 exports.handleCallback = async (req, res, next) => {
-	const { message, value } = req.body;
+	const { message, value, actions } = req.body;
 	const cid = Number(message.conversation_id);
+
+	const conv = await api.getConversation(cid);
 
 	const parsedValue = JSON.parse(value);
 	console.dir(parsedValue);
 
 	switch (parsedValue.action) {
+		case "modal_all_posts":
+		case "modal_my_posts":
+			await conv.sendMessage(`Post: ${actions.open_post}`);
+			break;
 		case undefined:
 		default:
-			await libKakaoWork.sendMessage({
-				conversationId: cid,
-				text: "Hello",
-				blocks: messages.blockPresets.welcome(2).serialize()
-			});
+			await conv.sendMessage(
+				'', messages.blockPresets.welcome(2)
+			);
+			break;
 	}
 	res.json({});
 };
--- handler/qna/messages.js
+++ handler/qna/messages.js
@@ -1,14 +1,30 @@
-const ButtonCallModal = require("kakaowork/dist/models/actions/ButtonCallModal").default;
-const ButtonSubmitAction = require('kakaowork/dist/models/actions/ButtonSubmitAction').default;
-const { BlockContainer, HeaderBlock, TextBlock, ButtonBlock, DividerBlock, ActionBlock } = require("kakaowork");
+const {
+    ModalContainer,
+    BlockContainer,
+    HeaderBlock,
+    TextBlock,
+    ButtonBlock,
+    DividerBlock,
+    ActionBlock,
+    LabelBlock,
+    SelectBlock,
+    SelectOption,
+    actions
+} = require("kakaowork");
 
 const defaultEntry = (obj) => {
     obj.type = "qna";
     return obj;
 };
 
+const toMain = new ButtonBlock(
+    '메인으로 돌아가기', 'default',
+    new actions.ButtonSubmitAction('to_main', '{"type": "main"}')
+);
+
 exports.blockPresets = {
     welcome: (postCount) => new BlockContainer(
+        'SWM12 QnA 게시판',
         new HeaderBlock("SWM12 QnA 게시판"),
         new TextBlock("프로젝트를 하다가 궁금한 점이 생기셨나요? 아래 게시판에서 다른 연수생 분들과 고민을 나누어보아요."),
         new TextBlock(`현재 게시판에 ${postCount}개의 글이 있어요.`),
@@ -16,24 +32,35 @@ exports.blockPresets = {
         new TextBlock("조회하기"),
         new ActionBlock(
             new ButtonBlock("전체 게시글", "default",
-                new ButtonCallModal(`{"type": "qna", "action": "modal_all_posts"}`)
+                new actions.ButtonCallModal(`{"type": "qna", "action": "modal_all_posts"}`)
             ),
             new ButtonBlock("내 게시글", "default",
-                new ButtonCallModal(`{"type": "qna", "action": "modal_my_posts"}`)
+                new actions.ButtonCallModal(`{"type": "qna", "action": "modal_my_posts"}`)
             )
         ),
         new TextBlock("작성하기"),
         new ActionBlock(
             new ButtonBlock("새 질문", "default",
-                new ButtonSubmitAction("qna_welcome", JSON.stringify(defaultEntry({
+                new actions.ButtonSubmitAction("qna_welcome", JSON.stringify(defaultEntry({
                     action: "new_question"
                 })))
             ),
             new ButtonBlock("새 답변", "default",
-                new ButtonSubmitAction("qna_welcome", JSON.stringify(defaultEntry({
+                new actions.ButtonSubmitAction("qna_welcome", JSON.stringify(defaultEntry({
                     action: "new_reply"
                 })))
             )
-        )
+        ),
+        toMain
+    ),
+    modal_all_posts: (posts) => new ModalContainer(
+        'QnA 전체 게시글', '확인', '취소', `{"type": "qna", "action": "modal_all_posts"}`,
+        new LabelBlock('전체 게시글'),
+        new SelectBlock('open_post', posts, true, '게시글 선택')
+    ),
+    modal_my_posts: (posts) => new ModalContainer(
+        'QnA 내 게시글', '확인', '취소', `{"type": "qna", "action": "modal_my_posts"}`,
+        new LabelBlock('내 게시글'),
+        new SelectBlock('open_post', posts, true, '게시글 선택')
     )
 }
