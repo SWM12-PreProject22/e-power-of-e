0b992bff82fa39a052e4166061b4b63e9033d35b
Eunhak Lee <lee@enak.kr>
2021-04-27 16:26:48 +0900

    feat: 게시글 등록, 조회 기능

--- handler/qna/index.js
+++ handler/qna/index.js
@@ -1,6 +1,8 @@
 const libKakaoWork = require('../../libs/kakaoWork');
-const { api } = require('../../libs/kakaoWork/wrapper');
-const { SelectOption } = require('kakaowork');
+const {api} = require('../../libs/kakaoWork/wrapper');
+const gql = require('./gql');
+
+const {SelectOption} = require('kakaowork');
 const messages = require("./messages");
 
 const mapper = (obj) => {
@@ -8,36 +10,50 @@ const mapper = (obj) => {
 	return Object.fromEntries(obj.serialize(map));
 }
 
+const cachedQNA = new Map();
+
 exports.handleRequest = async (req, res, next) => {
-	const { message, value } = req.body;
+	const {message, value, react_user_id} = req.body;
 	const parsedValue = JSON.parse(value);
 	console.dir(parsedValue);
 
 	switch (parsedValue.action) {
-		case "modal_all_posts":
-			res.json({
-				view: mapper(messages.blockPresets.modal_all_posts([
-					new SelectOption('게시글 1', '1'),
-					new SelectOption('게시글 2', '2'),
-					new SelectOption('게시글 3', '3')
-				]))
+		case "modal_all_posts": {
+			const data = (await gql.getAllQNA()).map((q) => {
+				const {title, content} = q;
+				cachedQNA.set(q.qnaId, {title, content});
+				return new SelectOption(q.title, q.qnaId);
 			});
+
+			res.json({
+				view: mapper(messages.blockPresets.modal_all_posts(data))
+			});
+		}
 			return;
-		case "modal_my_posts":
-			res.json({
-				view: mapper(messages.blockPresets.modal_my_posts([
-					new SelectOption('게시글 1', '1'),
-					new SelectOption('게시글 3', '3')
-				]))
+		case "modal_my_posts": {
+			const data = (await gql.getQNAByUserId(react_user_id)).map((q) => {
+				const {title, content} = q;
+				cachedQNA.set(q.qnaId, {title, content});
+				return new SelectOption(q.title, q.qnaId);
 			});
+
+			res.json({
+				view: mapper(messages.blockPresets.modal_my_posts(data))
+			});
+		}
+			return;
+		case 'new_question':
+			res.json({
+				view: mapper(messages.blockPresets.new_question())
+			})
 			return;
 		default:
-  			res.json({});
+			res.json({});
 	}
 };
 
 exports.handleCallback = async (req, res, next) => {
-	const { message, value, actions } = req.body;
+	const {message, value, actions, react_user_id} = req.body;
 	const cid = Number(message.conversation_id);
 
 	const conv = await api.getConversation(cid);
@@ -47,13 +63,46 @@ exports.handleCallback = async (req, res, next) => {
 
 	switch (parsedValue.action) {
 		case "modal_all_posts":
-		case "modal_my_posts":
-			await conv.sendMessage(`Post: ${actions.open_post}`);
+		case "modal_my_posts": {
+			const post = cachedQNA.get(actions.open_post);
+			if (post === undefined) {
+				await conv.sendMessage('', messages.blockPresets.no_post_found(actions.open))
+				break;
+			} else {
+				const {title, content} = post;
+				// TODO: 게시글에 답변(comment)이 있으면 답변 보기 버튼 띄우게
+				// TODO: 다른 게시글 보기 버튼
+				// TODO: 내 게시글인 경우 질문 마감하기 버튼
+				conv.sendMessage('', messages.blockPresets.view_post(title, content))
+					.catch((e) => {
+						console.dir(e.response.config.data);
+						console.dir(e.response.data.error);
+					});
+			}
+		}
+			break;
+		case 'new_question': {
+			const {title, text} = actions;
+			const {errors, data} = await gql.writeQNA(react_user_id, title, text);
+
+			if (errors !== undefined) {
+				console.dir(errors)
+				await conv.sendMessage(`게시글을 등록하는데 오류가 발생했습니다 ㅠㅠ`)
+					.catch((e) => {
+						console.dir(e.response.config.data);
+						console.dir(e.response.data.error);
+					})
+			}
+
+			// TODO: block 으로 바꾸기
+			else
+				await conv.sendMessage(`게시글이 등록되었습니다!`)
+		}
 			break;
 		case undefined:
 		default:
 			await conv.sendMessage(
-				'', messages.blockPresets.welcome(2)
+				'', messages.blockPresets.welcome((await gql.getAllQNA()).length)
 			);
 			break;
 	}
--- handler/qna/messages.js
+++ handler/qna/messages.js
@@ -9,6 +9,7 @@ const {
     LabelBlock,
     SelectBlock,
     SelectOption,
+    InputBlock,
     actions
 } = require("kakaowork");
 
@@ -16,6 +17,11 @@ const defaultEntry = (obj) => {
     obj.type = "qna";
     return obj;
 };
+
+const escapeEmpty = (blocks, hint) => {
+    if (!blocks.length) return [new SelectOption(hint, 'should_be_ignored')];
+    else return blocks;
+}
 
 const toMain = new ButtonBlock(
     '메인으로 돌아가기', 'default',
@@ -41,9 +47,7 @@ exports.blockPresets = {
         new TextBlock("작성하기"),
         new ActionBlock(
             new ButtonBlock("새 질문", "default",
-                new actions.ButtonSubmitAction("qna_welcome", JSON.stringify(defaultEntry({
-                    action: "new_question"
-                })))
+                new actions.ButtonCallModal(`{"type": "qna", "action": "new_question"}`)
             ),
             new ButtonBlock("새 답변", "default",
                 new actions.ButtonSubmitAction("qna_welcome", JSON.stringify(defaultEntry({
@@ -56,11 +60,35 @@ exports.blockPresets = {
     modal_all_posts: (posts) => new ModalContainer(
         'QnA 전체 게시글', '확인', '취소', `{"type": "qna", "action": "modal_all_posts"}`,
         new LabelBlock('전체 게시글'),
-        new SelectBlock('open_post', posts, true, '게시글 선택')
+        new SelectBlock('open_post', escapeEmpty(posts, '게시글이 없음'), true, '게시글 선택')
     ),
     modal_my_posts: (posts) => new ModalContainer(
         'QnA 내 게시글', '확인', '취소', `{"type": "qna", "action": "modal_my_posts"}`,
         new LabelBlock('내 게시글'),
-        new SelectBlock('open_post', posts, true, '게시글 선택')
+        new SelectBlock('open_post', escapeEmpty(posts, '게시글이 없음'), true, '게시글 선택')
+    ),
+    new_question: () => new ModalContainer(
+        '새 질문 작성하기', '확인', '취소', `{"type": "qna", "action": "new_question"}`,
+        new LabelBlock('*새 게시글 작성하기*'),
+        new LabelBlock(
+            '모든 게시글은 모니터링되며, 부적절한 게시글은 예고 없이 삭제될 수 있습니다. ' +
+            '게시글에는 작성자의 정보가 드러나지 않으니 필요하다면 게시글에 개인정보를 담지 않게 조심해주세요.'
+        ),
+        new InputBlock('title', true, '제목 (최대 20자)'),
+        new InputBlock('text', true, '내용을 입력해주세요 (최대 500자)\n크기가 자동으로 조절됩니다.')
+    ),
+    no_post_found: (hint) => new BlockContainer(
+        '게시글을 찾을 수 없음',
+        new HeaderBlock('SWM12'),
+        new TextBlock(`해당 게시글을 찾을 수 없습니다.\n검색 키워드: ${hint}`)
+    ),
+    view_post: (title, content) => new BlockContainer(
+        'SWM12 게시글',
+        new HeaderBlock(`게시글 - ${title}`),
+        new TextBlock(content),
+        // new DividerBlock(),
+        // new TextBlock('등록일: {시간}'),
+        new DividerBlock(),
+        toMain
     )
 }
